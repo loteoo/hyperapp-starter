74d0994aafb5934b17c3611a48568dafa27e45fc
Alexandre Lotte <alexlotte16@gmail.com>
2018-08-05 02:24:24 -0400

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,11 @@ module.exports = {
     'rules': {
       'no-unused-vars': [2, { 'varsIgnorePattern': 'h' }],
       'object-curly-spacing': [2, 'never'],
-      'no-use-before-define': 0
-    },
+      'no-multiple-empty-lines': 0,
+      'no-use-before-define': 0,
+      'operator-linebreak': 0,
+      'no-unused-vars': 0,
+      'padded-blocks': 0,
+      'no-undef': 0
+    }
 };
