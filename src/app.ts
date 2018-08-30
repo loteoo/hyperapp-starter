5dccc52da9fe7e5487fc87ea2289869c9a8d67f4
Alexandre Lotte <alexlotte16@gmail.com>
2018-08-30 16:42:43 -0400

    get typescript goin

--- src/app.ts
+++ src/app.ts
@@ -4,5 +4,7 @@ import {state} from './state/'
 import {actions} from './actions/'
 import {view} from './views/'
 
-window.main = app(state, actions, view, document.body)
-window.main.init()
+window.main = app(state, actions, view, document.body);
+window.main.init();
+
+
