49008bcbc69ce70711efed7f2b3a48898adcdb7f
Alexandre Lotte <alexlotte16@gmail.com>
2018-08-31 12:05:50 -0400

    adjusts actions

--- src/app/view.jsx
+++ src/app/view.jsx
@@ -5,11 +5,13 @@ import './style.css'
 // Import dependencies
 import {h} from 'hyperapp'
 
+import {FormComponent} from '../components/FormComponent'
 // Root view
 export const view = (state, actions) => (
   <main>
     <h1>Hyperapp!</h1>
     <h4>State: </h4>
     <pre>{JSON.stringify(state, null, 2)}</pre>
+    <FormComponent {...state.formComponent} />
   </main>
 )
