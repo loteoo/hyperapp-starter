d00949de381466066249bcbbb4d5d0cdea0641c4
Alexandre Lotte <alexlotte16@gmail.com>
2018-08-31 11:13:22 -0400

    test injections

--- src/app/view.jsx
+++ src/app/view.jsx
@@ -5,11 +5,13 @@ import './style.css'
 // Import dependencies
 import {h} from 'hyperapp'
 
+// import {TestComponent} from '../components/TestComponent'
 // Root view
 export const view = (state, actions) => (
   <main>
     <h1>Hyperapp!</h1>
     <h4>State: </h4>
     <pre>{JSON.stringify(state, null, 2)}</pre>
+    {/* <TestComponent {...state.testComponent} /> */}
   </main>
 )
