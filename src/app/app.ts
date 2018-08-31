5e37b232fee7907d0a2681a6af45e3c2c19071fc
Alexandre Lotte <alexlotte16@gmail.com>
2018-08-30 22:49:45 -0400

    structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,8 @@
 // App dependencies
 import {app} from 'hyperapp'
-import {state} from './state/'
-import {actions} from './actions/'
-import {view} from './views/'
+import {state} from './state'
+import {actions} from './actions'
+import {view} from './view'
 
 window.main = app(state, actions, view, document.body);
 window.main.init();
