70b9dd2a9b44966c7114794b4e83b6e15c0daaa1
Alexandre Lotte <alexlotte16@gmail.com>
2018-08-31 07:33:42 -0400

    rename generators & config

--- src/app/view.tsx
+++ src/app/view.tsx
@@ -5,11 +5,14 @@ import './style.css'
 // Import dependencies
 import {h} from 'hyperapp'
 
+
+
 // Root view
 export const view = (state, actions) => (
   <main>
     <h1>Hyperapp!</h1>
     <h4>State: </h4>
     <pre>{JSON.stringify(state, null, 2)}</pre>
+    <button onclick={ev => actions.set({count: (state.count || 0) + 1})}>+</button>
   </main>
 )
