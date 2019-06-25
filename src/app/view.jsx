a6a705411c006affbe2f5d474d1d8ac0ff1fd5ba
Alexandre Lotte <alexlotte16@gmail.com>
2019-06-25 19:56:58 -0400

    cleanup

--- src/app/view.jsx
+++ src/app/view.jsx
@@ -2,14 +2,9 @@ import { SetA, SetB } from './actions'
 
 const decodeInput = event => parseInt(event.target.value || 0)
 
-const container = {
-  maxWidth: '768px',
-  margin: '3rem auto'
-}
-
 // Root application view
 export default state => (
-  <main style={container}>
+  <main class="container">
     <h1>Do more with less</h1>
     <input type="number" value={state.a} oninput={[SetA, decodeInput]} />
     <input type="number" value={state.b} oninput={[SetB, decodeInput]} />
